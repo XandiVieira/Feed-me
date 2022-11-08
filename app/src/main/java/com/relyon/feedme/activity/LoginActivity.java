7ddb6bc8f1423c178c0e440ce0634624f41ace35
Alexandre <xandivieira@outlook.com>
2022-11-08 10:51:34 -0300

    Create Google login process

--- app/src/main/java/com/relyon/feedme/activity/LoginActivity.java
+++ app/src/main/java/com/relyon/feedme/activity/LoginActivity.java
@@ -1,35 +1,90 @@
 package com.relyon.feedme.activity;
 
 import android.app.Activity;
-
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProvider;
-
+import android.content.Intent;
 import android.os.Bundle;
-
-import androidx.annotation.Nullable;
-import androidx.annotation.StringRes;
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.text.Editable;
-import android.text.TextWatcher;
-import android.view.KeyEvent;
 import android.view.View;
-import android.view.inputmethod.EditorInfo;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ProgressBar;
-import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.GoogleAuthProvider;
 import com.relyon.feedme.R;
 import com.relyon.feedme.databinding.ActivityLoginBinding;
 
 public class LoginActivity extends AppCompatActivity {
 
+    ActivityLoginBinding binding;
+    private GoogleSignInClient googleSignInClient;
+    private FirebaseAuth mAuth;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        binding = ActivityLoginBinding.inflate(getLayoutInflater());
+
+        View view = binding.getRoot();
+        setContentView(view);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(
+                GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+
+        googleSignInClient = GoogleSignIn.getClient(this, gso);
+
+        binding.googleButton.setOnClickListener(viewBt -> signIn());
+    }
+
+    private void signIn() {
+        Intent intent = googleSignInClient.getSignInIntent();
+        openActivity.launch(intent);
+    }
+
+    ActivityResultLauncher<Intent> openActivity = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            result -> {
+                if (result.getResultCode() == Activity.RESULT_OK) {
+                    Intent data = result.getData();
+                    Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+                    try {
+                        GoogleSignInAccount acc = task.getResult(ApiException.class);
+                        loginGoogle(acc.getIdToken());
+                    } catch (ApiException e) {
+                        Toast.makeText(this, "Fail", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+    );
+
+    private void loginGoogle(String token) {
+        AuthCredential credential = GoogleAuthProvider.getCredential(token, null);
+        mAuth.signInWithCredential(credential).addOnCompleteListener(task -> {
+            if (task.isSuccessful()) {
+                homeActivity();
+                finish();
+            } else {
+                Toast.makeText(this, "Error", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void homeActivity() {
+        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/relyon/feedme/activity/MainActivity.java
+++ app/src/main/java/com/relyon/feedme/activity/MainActivity.java
@@ -1,16 +1,62 @@
 package com.relyon.feedme.activity;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
-import com.relyon.feedme.R;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.firebase.auth.FirebaseAuth;
+import com.relyon.feedme.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
+
+    private FirebaseAuth mAuth;
+    private GoogleSignInClient googleSignInClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        mAuth = FirebaseAuth.getInstance();
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(
+                GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+
+        googleSignInClient = GoogleSignIn.getClient(this, gso);
+
+        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
+
+        if (account != null) {
+            String name = account.getDisplayName();
+            String email = account.getEmail();
+            binding.email.setText(email);
+            binding.name.setText(name);
+        }
+
+        binding.logout.setOnClickListener(tv -> {
+            logout();
+        });
+
+
+    }
+
+    private void logout() {
+        googleSignInClient.signOut().addOnCompleteListener(task -> {
+            mAuth.signOut();
+            finish();
+            startActivity(new Intent(getApplicationContext(), LoginActivity.class));
+        });
     }
 }
